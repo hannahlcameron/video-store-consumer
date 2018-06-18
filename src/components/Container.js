5ad6dddaccd280dedb95e8d864709ff2268810dd
Brenda Rios <riosbrenda@gmail.com>
2018-06-18 16:22:23 -0700

    working on passing library data to customer and movie collection to each component.

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import Movie from './Movie'
-import Customer from './Customer'
+import MovieCollection from './MovieCollection'
+import CustomerCollection from './CustomerCollection'
 
 URL = 'http://localhost:3000/';
 class Container extends Component {
 
-getMovies() {
+constructor() {
+  super();
+  this.state = {
+    movies: []
+  }
+}
+
+componentDidMount() {
+// getMovies() {
   axios.get(`${URL}/movies`)
   .then((response) => {
-    console.log(response);
+    console.log(response.data);
+const all_movies = response.data.map((movie)=>{
+  return movie
+});
+
+    this.setState({
+      movies: all_movies
+    })
   })
   .catch((error)=> {
     console.log(error);
@@ -20,7 +35,10 @@ getMovies() {
 getCustomers() {
   axios.get(`${URL}/customers`)
   .then((response) => {
-    console.log(response);
+    console.log(response.data);
+    return (
+      response.data
+    )
   })
   .catch((error)=> {
     console.log(error);
@@ -28,14 +46,15 @@ getCustomers() {
 }
 
   render() {
-    const movies = this.getMovies();
-    const customers = this.getCustomers();
+    // const movies = this.getMovies();
+    // const customers = this.getCustomers();
     return (
       <div>
-      <Customer />
-      <Movie />
-      {movies}
-      {customers}
+      <p>here?</p>
+        <CustomerCollection test = "this is a test" />
+        <MovieCollection movies = {this.state.movies}  />
+
+
       </div>
 
     );
