5bca8025b0b0732baddd1610a1e9049ddce1133e
Brenda Rios <riosbrenda@gmail.com>
2018-06-18 15:20:44 -0700

    added functions to get movies and get customers from the api.

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,13 +4,38 @@ import axios from 'axios';
 import Movie from './Movie'
 import Customer from './Customer'
 
-
+URL = 'http://localhost:3000/';
 class Container extends Component {
+
+getMovies() {
+  axios.get(`${URL}/movies`)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error)=> {
+    console.log(error);
+  })
+}
+
+getCustomers() {
+  axios.get(`${URL}/customers`)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error)=> {
+    console.log(error);
+  })
+}
+
   render() {
+    const movies = this.getMovies();
+    const customers = this.getCustomers();
     return (
       <div>
       <Customer />
       <Movie />
+      {movies}
+      {customers}
       </div>
 
     );
