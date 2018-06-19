19bff8d30ca7cb9be133599a4896281922015c68
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-06-19 13:54:41 -0700

    added a callback to be able to set state in container for customer and movies

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,22 +1,71 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import axios from "axios";
+import Movie from "./Movie";
 
-
+const URL = 'http://localhost:3000/';
 
 class SearchForm extends Component {
   // static propTypes = {
   //   name: PropTypes.string.isRequired
   // }
-  render() {
-    return (
-      <form  >
-            <label htmlFor='query' className="new-card-form__form-label">Search for:</label>
-            <textarea name='query'/>
-            <button type="submit" className="new-card-form__form-button">Submit</button>
-          </form>
-    );
+
+  constructor(){
+    super();
+    this.state = {
+      searchResult: [],
+      query: ""
+    }
   }
-}
+
+  searchForResults = (submit) =>{
+    submit.preventDefault();
+    axios.get(`${URL}/movies?query=${this.state.query}`)
+    .then((response) => {
+      console.log(response.data);
+
+      const results = response.data;
+
+      this.setState({
+        searchResult: results
+      })
+    })
+    .catch((error)=> {
+      console.log(error);
+    })}
+
+    onInputChange = (event) => {
+
+      this.setState({
+        query: event.target.value
+      })
+
+    }
 
 
-export default SearchForm;
+    render() {
+      //
+      // const each_movie = this.state.searchResult.map((movie, index)=>{
+      //   console.log(movie.title);
+      //   return <Movie key={index} title={movie.title} />
+      // })
+
+      return (
+        <div>
+          <form onSubmit={this.searchForResults} >
+            <label htmlFor='query' className="new-card-form__form-label">Search for:</label>
+            <textarea
+            name='query'
+            value= {this.state.query}
+            onChange={this.onInputChange}/>
+            <button type="submit" className="new-card-form__form-button">Submit</button>
+            </form>
+            <ul>
+            </ul>
+        </div>
+      );
+    }
+  }
+
+
+  export default SearchForm;
