a6b43c4a92d4805f239612cb9bde8c3b6d5ebb15
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-06-20 15:38:35 -0700

    did some css

--- src/components/Container.js
+++ src/components/Container.js
@@ -62,61 +62,70 @@ class Container extends Component {
     .then((response)=>{
       this.updateStatus(`Successfully checked out ${this.state.selectedMovieTitle} for
         ${this.state.selectedCustomerName}`, 'success')
-      console.log(response);
-    })
-    .catch((error)=>{
-      this.updateStatus(error.message,'error');
-    })
+        console.log(response);
+      })
+      .catch((error)=>{
+        this.updateStatus(error.message,'error');
+      })
 
-  }
-
-
-  render() {
-
-    return (
-      <main>
-      <Status
-      message={this.state.status.message}
-      type={this.state.status.type}/>
-      <Router>
-      <div>
-      <h1>Selected Customer {this.state.selectedCustomerName}</h1>
-      <h1>Selected Movie {this.state.selectedMovieTitle}</h1>
-      <button onClick={this.makeRental}>Check out new Rental</button>
-      <ul>
-      <li>
-      <Link to="/">Home</Link>
-      </li>
-      <li>
-      <Link to="/search">Search</Link>
-      </li>
-      <li>
-      <Link to="/library">Library</Link>
-      </li>
-      <li>
-      <Link to="/customers">Customers</Link>
-      </li>
-
-      </ul>
-
-      <hr />
-
-
-      <Route exact path="/search" render={props => <SearchForm
-        callbackUpdateStatus = {this.updateStatus} />}/>
-      <Route exact path="/library" render={props => <MovieCollection
-        callbackgetSelectedMovie = {this.getSelectedMovie}
-        callbackUpdateStatus = {this.updateStatus}  />} />
-        <Route path="/customers" render={props => <CustomerCollection
-          callbackgetSelectedCustomer = {this.getSelectedCustomer}
-          callbackUpdateStatus = {this.updateStatus} />} />
-
-          </div>
-          </Router>
-          </main>
-        );
-      }
     }
 
 
-    export default Container;
+    render() {
+
+      return (
+        <main>
+
+          <Router>
+            <div>
+            <nav className="top-bar">
+              <Status
+              message={this.state.status.message}
+              type={this.state.status.type}
+              className="status"/>
+
+
+              <div className="selection">
+                <h1>Selected Customer {this.state.selectedCustomerName}</h1>
+                <h1>Selected Movie {this.state.selectedMovieTitle}</h1>
+              </div>
+              <button onClick={this.makeRental} className="rental-button">Check out new Rental</button>
+              <ul className="nav">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/search">Search</Link>
+                  </li>
+                  <li>
+                    <Link to="/library">Library</Link>
+                  </li>
+                  <li>
+                    <Link to="/customers">Customers</Link>
+                  </li>
+
+              </ul>
+
+            </nav>
+
+        <hr />
+
+
+        <Route exact path="/search" render={props => <SearchForm
+          callbackUpdateStatus = {this.updateStatus} />}/>
+        <Route exact path="/library" render={props => <MovieCollection
+            callbackgetSelectedMovie = {this.getSelectedMovie}
+            callbackUpdateStatus = {this.updateStatus}  />} />
+        <Route path="/customers" render={props => <CustomerCollection
+              callbackgetSelectedCustomer = {this.getSelectedCustomer}
+              callbackUpdateStatus = {this.updateStatus} />} />
+
+              </div>
+        </Router>
+      </main>
+            );
+          }
+        }
+
+
+        export default Container;
