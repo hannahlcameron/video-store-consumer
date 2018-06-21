06b8b23e83db6ccdfc1b35d8906e4ba16a5a7708
Hannah Cameron <hannahiscoding@gmail.com>
2018-06-21 15:32:07 -0700

    attempted changed to page layout

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -47,11 +47,11 @@ else {
 
     return (
       <li className="movie-container">
-      <h3 className="title">{this.props.title}</h3>
-      <img src={this.props.image} alt={`cover art for ${this.props.title}`} className="image"/>
-      <p className="release-date">{this.props.release_date}</p>
-      <p className="overview">{this.props.overview}</p>
-      {button}
+        <h3 className="title">{this.props.title}</h3>
+        <img src={this.props.image} alt={`cover art for ${this.props.title}`} className="image"/>
+        <p className="release-date">{this.props.release_date}</p>
+        <p className="overview">{this.props.overview}</p>
+        {button}
       </li>
     );
   }
--- src/components/MovieCollection.js
+++ src/components/MovieCollection.js
@@ -51,12 +51,11 @@ class MovieCollection extends Component {
     })
 
     return (
-      <div>
-
-      <h1> Movies </h1>
-      <ul className="movies">
-      {each_movie}
-      </ul>
+      <div className="movie-collection">
+        <h3> Movies </h3>
+        <ul className="movies">
+          {each_movie}
+        </ul>
       </div>
     );
   }
